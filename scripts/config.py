5433fb2634d42e33b9fa5530ad6650ca4b9dcfa0
daz-sports <dacimc9@gmail.com>
2025-10-06 19:16:07 +0100

    Incorrect user instructions

--- scripts/config.py
+++ scripts/config.py
@@ -9,9 +9,9 @@ class Config:
     
     # --- Input Data Paths ---
     CRICSHEET_JSON_DIR: str = "path/to/cricsheet_json_files"
-    REGISTRY_CSV_PATH: str = ""path/to/registry_100725.csv"
-    TEAMS_CSV_PATH: str = ""path/to/teams_sample.csv"
-    VENUES_CSV_PATH: str = ""path/to/venues_sample.csv"
-    VENUE_ALIASES_CSV_PATH: str = ""path/to/venue_aliases_sample.csv"
-    PLAYERS_CSV_PATH: str = ""path/to/players_info_sample.csv"
-    OFFICIALS_CSV_PATH: str = ""path/to/officials_sample.csv"
+    REGISTRY_CSV_PATH: str = "path/to/registry_100725.csv"
+    TEAMS_CSV_PATH: str = "path/to/teams_sample.csv"
+    VENUES_CSV_PATH: str = "path/to/venues_sample.csv"
+    VENUE_ALIASES_CSV_PATH: str = "path/to/venue_aliases_sample.csv"
+    PLAYERS_CSV_PATH: str = "path/to/players_info_sample.csv"
+    OFFICIALS_CSV_PATH: str = "path/to/officials_sample.csv"
