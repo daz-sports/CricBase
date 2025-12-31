268d9e3dfe0c38dea860c47a34c40d2c3f6f2702
daz-sports <dacimc9@gmail.com>
2025-12-31 15:56:18 +0000

    Added the new additional_json_directory parameter to enable uploading user-created json files. Also added the parameters need to run the ICC scraper and build the missing_matches table.

--- scripts/config.py
+++ scripts/config.py
@@ -9,10 +9,17 @@ class Config:
     SCHEMA_VERSION: str = "1.0.0"
 
     # --- Input Data Paths ---
-    CRICSHEET_JSON_DIR: str = "path/to/cricsheet_json_files"
+    CRICSHEET_JSON_DIR: str = "path/to/t20s_json_sample/"
+    ADDITIONAL_JSON_DIR: str = "path/to/abandoned_pre_start/"
     REGISTRY_CSV_PATH: str = "path/to/registry_100725.csv"
     TEAMS_CSV_PATH: str = "path/to/teams_sample.csv"
     VENUES_CSV_PATH: str = "path/to/venues_sample.csv"
     VENUE_ALIASES_CSV_PATH: str = "path/to/venue_aliases_sample.csv"
     PLAYERS_CSV_PATH: str = "path/to/players_info_sample.csv"
     OFFICIALS_CSV_PATH: str = "path/to/officials_sample.csv"
+
+    # --- ICC Scraper Settings ---
+    SCRAPE_START_YEAR: int = 2024
+    SCRAPE_START_MONTH: int = 1
+    SCRAPE_END_YEAR: int = 2024
+    SCRAPE_END_MONTH: int = 12
